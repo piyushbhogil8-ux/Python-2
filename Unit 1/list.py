455bd817188fbbbaba703813250a3c244ddd9291
piyushbhogil8-ux <piyushbhogil8@gmail.com>
2026-03-25 23:29:22 +0530

    Create list.py

---
+++ Unit 1/list.py
@@ -0,0 +1,34 @@
+my_list = [10, 20, 40, 30]
+
+#access the list element
+print(my_list[2])
+
+#add and remove list element
+
+#Adding 
+my_list.append(50)
+print(my_list)
+
+my_list.insert(1,15)
+print(my_list)
+
+my_list.extend([60,70,80])
+print(my_list)
+
+#Removing
+my_list.remove(15)
+print(my_list)
+
+my_list.pop(7)
+print(my_list)
+
+del my_list [6]
+print(my_list)
+ 
+#sort list element
+my_list.sort()
+print(my_list)
+
+# reverse list element 
+my_list.reverse()
+print(my_list)
