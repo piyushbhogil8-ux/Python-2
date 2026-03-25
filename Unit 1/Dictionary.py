e8f3b0a8d49840f83dc649542988b27ab11c88e9
piyushbhogil8-ux <piyushbhogil8@gmail.com>
2026-03-25 23:20:29 +0530

    Update Dictionary.py

--- Unit 1/Dictionary.py
+++ Unit 1/Dictionary.py
@@ -1,1 +1,25 @@
+# creating and accessing the element:
+my_dict = {
+    "name": "ABC",
+    "age": 19,
+    "city": "LONDON"
+}
+print(my_dict)
 
+print(my_dict["name"])
+print(my_dict["age"])
+print(my_dict["city"])
+
+#update dictionary:
+my_dict['age']=20
+print(my_dict)
+
+# removing element:
+del my_dict['city']
+print(my_dict)
+
+# merging dictionary:
+dict1 = {'a':1}
+dict2 = {'b':2}
+dict1.update(dict2)
+print(dict1)
